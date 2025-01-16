f51dfbf4a0f2063e00ae889169caa117294d425d
Manukrishnaks <manukrishnamanu02@gmail.com>
2025-01-16 11:28:23 +0530

    Food_delivery

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://manukrishna:00000000@cluster0.gcn5f.mongodb.net/food');
+    await mongoose.connect(`mongodb+srv://manukrishna:00000000@cluster0.gcn5f.mongodb.net/food`);
     console.log("DB Connected");
   } catch (error) {
     console.error("DB Connection Error:", error.message);
